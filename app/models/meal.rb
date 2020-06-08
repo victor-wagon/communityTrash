c479ba3ac25880906710cc151ee6015ae69eb9d9
Victor Guillemot <victor.guillemot.wagon@gmail.com>
2020-06-08 14:03:37 +0200

    added validations

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,5 @@
 class Meal < ApplicationRecord
   has_many :reviews, dependent: :destroy
   belongs_to :user
+  validates :title, :description, :rating, :user, :capacity, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :meal
+  validates :title, :content, :rating, :meal, presence: true
 end
